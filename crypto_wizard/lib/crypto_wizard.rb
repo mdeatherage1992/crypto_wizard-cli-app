6d01d7982b6202401b46d0a82941e75ff9608e32
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-07-05 17:44:13 +0000

    third commit

--- crypto_wizard/lib/crypto_wizard.rb
+++ crypto_wizard/lib/crypto_wizard.rb
@@ -4,3 +4,5 @@ require 'open-uri'
 require_relative "./crypto_wizard/version"
 require_relative "./crypto_wizard/cli"
 require_relative "./crypto_wizard/listings"
+require_relative "./crypto_wizard/scraper"
+require_relative "./crypto_wizard/menu"
