6c682adeea85726b47d287a28263fa059ae36fe3
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-07-05 22:36:50 +0000

    menu change

--- crypto_wizard/lib/crypto_wizard/cli.rb
+++ crypto_wizard/lib/crypto_wizard/cli.rb
@@ -3,7 +3,6 @@ class CryptoWizard::CLI
 
 def call
   puts "Welcome to the Cryptocurrency Wizard!"
-  list_currencies
   menu
 end
 
@@ -14,11 +13,33 @@ def list_currencies
   end
 end
 
+
 def menu
-  CryptoWizard::Menu.menu
+  input = nil
+  @listings = CryptoWizard::Listings.listing
+  while input != 'exit'
+    puts "please the # cryptocurrency that you would like to see, list to be shown listings, or exit to exit"
+    input = gets.strip.downcase
+    if input.to_i > 0 && input.to_i < 5
+      puts @listings[input.to_i - 1].name
+      puts "PRICE($):"
+      puts @listings[input.to_i - 1].price
+      puts "SUPPLY:"
+      puts @listings[input.to_i - 1].supply
+      puts "MARKET CAP($):"
+      puts @listings[input.to_i - 1].market_cap
+    elsif input == "list"
+      list_currencies
+    elsif input == "exit"
+      goodbye
+    else
+      puts "not sure what you mean, enter list for listings, or exit to exit"
+    end
+  end
 end
 
-def self.goodbye
+
+def goodbye
   puts "Have a good day"
 end
 
